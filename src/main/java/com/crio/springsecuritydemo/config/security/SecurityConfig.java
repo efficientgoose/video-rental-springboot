5bf84b97f512cb52492121d447974cb6d50f67a2
Abdul Rahman <abdul.rahman@criodo.com>
2024-03-30 00:45:30 +0530

    Removed unused imports

--- src/main/java/com/crio/springsecuritydemo/config/security/SecurityConfig.java
+++ src/main/java/com/crio/springsecuritydemo/config/security/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.crio.springsecuritydemo.config.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties.Authentication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
